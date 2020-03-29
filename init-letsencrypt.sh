a204297cd7500b7717f018391e1b6668c7672faa
RyuCoder <rockstar.ryucoder@gmail.com>
2020-03-29 23:53:15 +0530

    Domain Name Updated.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(suchetanzpartner.de www.suchetanzpartner.de)
+domains=(www.suchetanzpartner.de)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="sebastian.jung2@gmail.com" # Adding a valid address is strongly recommended
