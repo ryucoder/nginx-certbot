2ace9fa0e605c7ff202db6c20f5c2be5a82f47d1
RyuCoder <rockstar.ryucoder@gmail.com>
2020-03-31 02:33:59 +0530

    Changes.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(www.suchetanzpartner.de)
 rsa_key_size=4096
-data_path="./data/certbot"
+data_path="./certbot"
 email="sebastian.jung2@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
